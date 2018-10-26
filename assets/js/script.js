18a00e5d21a2e1b9d034a0e858f85284d0d73cfb
chuynh18 <38088640+chuynh18@users.noreply.github.com>
2018-10-26 16:59:37 -0700

    sound and smaller images

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,36 @@
 "use strict";
 
+// Immediately-invoked function expression (called via an onclick attribute in the img element) in the HTML
 var advanceImage = (function() {
+   // keep track of what picture and text to show
    var counter = -1;
 
+   // pictures and text to show
    var images = [
       {url: "assets/img/dorky.jpg", text: "Sometimes he can look very dorky."},
       {url: "assets/img/sneeze.jpg", text: "Sometimes he inhales grass and dirt."},
       {url: "assets/img/mysterious.jpg", text: "Sometimes he can look very mysterious."}
    ];
 
-   return function() {
-      counter++;
+   // audio files
+   var audio = [
+      new Audio("assets/snd/meow1.webm"),
+      new Audio("assets/snd/meow2.webm")
+   ];
 
+   return function() {
+      counter++; // increment counter
+
+      // reset counter to 0 when it reaches 3
       if (counter === 3) {
          counter = 0;
       }
    
+      // set the image and text
       document.getElementById("image").src = images[counter].url;
       document.getElementById("text").textContent = images[counter].text;
+
+      // play one of the two audio files randomly
+      audio[Math.floor(Math.random() * 2)].play();
    }
 })();
